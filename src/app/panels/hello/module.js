aae70fc17ce2180676b41f0f9a477c2ff9ee5f5b
Ross Huggett <ross.huggett@gmail.com>
2014-07-03 17:25:54 +0100

    ESF-381 - Added hello panel

---
+++ src/app/panels/hello/module.js
@@ -0,0 +1,40 @@
+/*
+  ## Fields (DEPRECATED)
+*/
+define([
+  'angular',
+  'app',
+  'lodash'
+],
+function (angular, app, _) {
+  'use strict';
+
+  var module = angular.module('kibana.panels.hello', []);
+  app.useModule(module);
+
+  module.controller('hello', function($scope) {
+
+    $scope.panelMeta = {
+      status  : "Stable",
+      description : "Hello"
+    };
+
+
+    // Set and populate defaults
+    var _d = {
+      style   : {},
+      arrange : 'vertical',
+    };
+    _.defaults($scope.panel,_d);
+
+    $scope.init = function() {
+      // Place holder until I remove this
+    };
+  });
+
+  module.filter('greet', function() {
+    return function(name) {
+        return 'Hello, ' + name + '!';
+    };
+  });
+});
